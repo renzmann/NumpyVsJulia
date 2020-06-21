ce33ac7607e5b062798c27a6b178e8a2ef217224
renzmann <robert.enzmann@antuit.com>
2020-06-21 10:31:05 -0500

    use @, not np.sum

--- simple_loop_np.py
+++ simple_loop_np.py
@@ -1,5 +1,5 @@
-import numpy as np
 import time
+import numpy as np
 
 
 VEC_SIZE = 1000
@@ -11,6 +11,6 @@ v = np.random.randint(1, 1000, VEC_SIZE)
 
 start = time.time()
 for i in range(NUM_RUNS):
-    np.sum(u * v)
+    u @ v
 end = time.time()
 print(f"{end - start}")
